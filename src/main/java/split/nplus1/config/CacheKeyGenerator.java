7d83e739d91b48294e5d56fac814eb28f16b886a
splitCoding <voicesplit@daum.net>
2023-11-12 00:01:27 +0900

    feat: KeyGenerator 를 구현한 CacheKeyGenerator 클래스 작성

---
+++ src/main/java/split/nplus1/config/CacheKeyGenerator.java
@@ -0,0 +1,12 @@
+package split.nplus1.config;
+
+import java.lang.reflect.Method;
+import org.springframework.cache.interceptor.KeyGenerator;
+
+public class CacheKeyGenerator implements KeyGenerator {
+
+    @Override
+    public Object generate(final Object target, final Method method, final Object... params) {
+        return method.getName() + "-" + params.length;
+    }
+}
